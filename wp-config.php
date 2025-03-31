d3b55ec2270dd43a4eafb4fd21b4fc3c640a73f5
Mikko Huotari <mikko.huotari@hiq.fi>
2025-03-31 12:46:54 +0300

    fix COOKIE_DOMAIN setting in wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -36,7 +36,7 @@ if ( ! function_exists( 'get_int_env_value' ) ) {
 define( 'WPO_SCIM_TOKEN', getenv( 'WORDPRESS_WPO_SCIM_TOKEN' ) );
 define( 'WPO_ALLOW_DEVELOPER_HOOKS', getenv( 'WORDPRESS_WPO_ALLOW_DEVELOPER_HOOKS' ) );
 define( 'O365_PROFILE_IMG_PATH', getenv( 'WORDPRESS_O365_PROFILE_IMG_PATH' ) );
-define( 'COOKIE_DOMAIN', 'WORDPRESS_COOKIE_DOMAIN');
+define( 'COOKIE_DOMAIN', getenv( 'WORDPRESS_COOKIE_DOMAIN' ) );
 
 /**
   * Environment
