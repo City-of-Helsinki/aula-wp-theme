52bccf53776825314c3def573752b5b59b158a17
Mikko Huotari <mikko.huotari@hiq.fi>
2025-04-01 15:27:52 +0300

    enable mysqli storage for wordfence

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,7 @@ define( 'WPO_SCIM_TOKEN', getenv( 'WORDPRESS_WPO_SCIM_TOKEN' ) );
 define( 'WPO_ALLOW_DEVELOPER_HOOKS', getenv( 'WORDPRESS_WPO_ALLOW_DEVELOPER_HOOKS' ) );
 define( 'O365_PROFILE_IMG_PATH', getenv( 'WORDPRESS_O365_PROFILE_IMG_PATH' ) );
 define( 'COOKIE_DOMAIN', getenv( 'WORDPRESS_COOKIE_DOMAIN' ) );
+define( 'WFWAF_STORAGE_ENGINE', 'mysqli' );
 
 /**
   * Environment
